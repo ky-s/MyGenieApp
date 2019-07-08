3150f3452d1c0aa7953031c9d2cec9e059a3281b
ky-s <kyohei_86@hotmail.com>
2019-07-09 00:50:00 +0900

    show_doneをファイルに記憶できるようにした

--- app/resources/tasks/TasksController.jl
+++ app/resources/tasks/TasksController.jl
@@ -8,7 +8,8 @@ using Dates
 
 function index()
     tasks = sort(SearchLight.all(Tasks.Task), by = t -> (t.done, t.deadline))
-    html!(:tasks, :index, tasks = tasks, today = Date(now()))
+    @show show_done()
+    html!(:tasks, :index, tasks = tasks, today = Date(now()), show_done = show_done())
 end
 
 function new()
@@ -40,6 +41,14 @@ end
 function ajax_destroy()
     SearchLight.find_one!!(Tasks.Task, @params(:task_id)) |> delete
     true
+end
+
+function ajax_change_show_done()
+    open(fp -> write(fp, string((@params(:show_done) == "true") + 0)), "show_done", "w")
+end
+
+function show_done()
+     open(fp -> read(fp, String), "show_done", "r") == "1"
 end
 
 end
