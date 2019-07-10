70af57994171abe86bcc4b1ef3b8a8f386fc7888
ky-s <kyohei_86@hotmail.com>
2019-07-10 21:07:47 +0900

    ajax_create

--- app/helpers/ViewHelper.jl
+++ app/helpers/ViewHelper.jl
@@ -14,11 +14,11 @@ function task_form(action; task::Tasks.Task = Tasks.Task(content = "", deadline 
     @show action
     id_field = isnull(task.id) ? "" : """<input type="hidden" name="task_id" value="$(get(task.id))"/>"""
     """
-    <form action="$(action)" method="POST">
+    <form action="$(action)" method="POST" id="task-form">
       <div class="input-group mb-3">
         $(id_field)
-        <input class="form-control" type="date" name="task_deadline" value="$(task.deadline)"/>
-        <input id="todo-content" class="form-control" type="text" name="task_content" placeholder="Write down your TODO" value="$(task.content)" style="width: 50%" />
+        <input id="task-deadline" class="form-control" type="date" name="task_deadline" value="$(task.deadline)"/>
+        <input id="task-content" class="form-control" type="text" name="task_content" placeholder="Write down your TODO" value="$(task.content)" style="width: 50%" />
         <div class="input-group-append">
           <input type="submit" value="$(isnull(task.id) ? "Create" : "Update")" class="btn btn-sm btn-primary"/>
         </div>
@@ -30,18 +30,18 @@ end
 function task_tr(task)
     today = Date(now())
     """
-        <tr id="tr_$(get(task.id))" data-id="$(get(task.id))" class=$(task.done ? "table-success" : "")>
+        <tr id="tr_$(get(task.id))" data-id="$(get(task.id))" class="$(task.done ? "table-success" : "")">
           <td>
             <input type="checkbox" id="$(get(task.id))" data-id="$(get(task.id))"
                    class="checkbox done-checkbox" $(task.done ? "checked" : "") />
           </td>
           <td>
-            <span id="deadline_$(get(task.id))" class=$(task.done ? "done" : task.deadline <= today ? "deadline" : "doing")>
+            <span id="deadline_$(get(task.id))" class="$(task.done ? "done" : task.deadline <= today ? "deadline" : "doing")">
                   $(task.deadline)
             </span>
           </td>
           <td>
-            <span id="content_$(get(task.id))" class=$(task.done ? "done" : task.deadline <= today ? "deadline" : "doing")>
+            <span id="content_$(get(task.id))" class="$(task.done ? "done" : task.deadline <= today ? "deadline" : "doing")">
                   $(task.content)
             </span>
           </td>
--- app/resources/tasks/TasksController.jl
+++ app/resources/tasks/TasksController.jl
@@ -8,30 +8,43 @@ using Dates
 
 using ViewHelper
 
+# index page
+#   - new_form      ... ajax_create form. The normal submit will be canceled by event.preventDefault().
+#   - show_done     ... show/hidden done task.
+#   - tasks_tr      ... Tasks List in table > tbody. included Edit and Destroy button.
+#   - today         ... Just today.
 function index()
     tasks = sort(SearchLight.all(Tasks.Task), by = t -> (t.done, t.deadline))
     tasks_tr = map(task_tr, tasks) |> join
-    html!(:tasks, :index, show_done = show_done(), new_form = task_form(link_to(:create_task)), tasks_tr = tasks_tr)
+    html!(:tasks, :index, show_done = show_done(), new_form = task_form(""), tasks_tr = tasks_tr, today = Date(now()))
 end
 
-function new()
+function new() # not used
     html!(:tasks, :new)
 end
 
-function create()
+function create() # not used
     Tasks.Task(content = @params(:task_content), done = false, deadline = Date(@params(:task_deadline))) |> save && redirect_to(:get_tasks)
 end
 
+# edit page
+#   - edit_form     ... Normal edit form.
 function edit()
     task = SearchLight.find_one!!(Tasks.Task, @params(:task_id))
     html!(:tasks, :edit, edit_form = task_form(link_to(:update_task), task = task))
 end
 
+# update action
 function update()
     task = SearchLight.find_one!!(Tasks.Task, @params(:task_id))
     task.content  = @params(:task_content)
     task.deadline = @params(:task_deadline)
     save(task) && redirect_to(:get_tasks)
+end
+
+function ajax_create()
+    task = Tasks.Task(content = @params(:task_content), done = false, deadline = Date(@params(:task_deadline))) |> save!
+    respond(Dict(:html => task_tr(task)))
 end
 
 function ajax_update_status()
@@ -45,8 +58,11 @@ function ajax_destroy()
     true
 end
 
+# ajax show_done changing.
+#   just write .show_done file.
+#   In julia, true is 1, false is 0.
 function ajax_change_show_done()
-    open(fp -> write(fp, string((@params(:show_done) == "true") + 0)), ".show_done", "w")
+    open(fp -> write(fp, string(+(@params(:show_done) == "true"))), ".show_done", "w")
 end
 
 function show_done()
